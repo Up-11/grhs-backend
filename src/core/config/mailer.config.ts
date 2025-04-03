23dd6042b358206895d24e6e04fc154802ad6664
up-11 <iurykolgin@gmail.com>
2025-04-03 23:15:17 +0700

    test: mail debug

--- src/core/config/mailer.config.ts
+++ src/core/config/mailer.config.ts
@@ -2,18 +2,30 @@ import type { MailerOptions } from '@nestjs-modules/mailer'
 import { ConfigService } from '@nestjs/config'
 
 export function getMailerConfig(configService: ConfigService): MailerOptions {
-	return {
+	const config = {
 		transport: {
 			host: configService.getOrThrow<string>('MAIL_HOST'),
-			port: configService.getOrThrow<string>('MAIL_PORT'),
+			port: parseInt(configService.getOrThrow<string>('MAIL_PORT')),
 			secure: false,
 			auth: {
 				user: configService.getOrThrow<string>('MAIL_LOGIN'),
 				pass: configService.getOrThrow<string>('MAIL_PASSWORD')
-			}
+			},
+			logger: true,
+			debug: true
 		},
 		defaults: {
-			from: ` "Green House Support"  ${configService.getOrThrow<string>('MAIL_LOGIN')}`
+			from: `"Green House Support" <${configService.getOrThrow<string>('MAIL_LOGIN')}>`
 		}
 	}
+
+	console.log('Mailer Config:', {
+		host: config.transport.host,
+		port: config.transport.port,
+		secure: config.transport.secure,
+		user: config.transport.auth.user,
+		pass: config.transport.auth.pass ? '***' : 'undefined'
+	})
+
+	return config
 }
